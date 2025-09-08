d1dcadaf59a1ad98f80cade6e28042d45626f624
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-09-09 05:44:44 +0700

    new file:   app/Services/MidtransService.php 	new file:   config/midtrans.php

---
+++ app/Services/MidtransService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use Midtrans\Config;
+
+class MidtransService {
+    public static function setup():void 
+    {
+        Config::$serverKey = config('midtrans.server_key');
+Config::$isProduction = filter_var(config('midtrans.is_production'), FILTER_VALIDATE_BOOLEAN);
+Config::$isSanitized = filter_var(config('midtrans.is_sanitized'), FILTER_VALIDATE_BOOLEAN);
+Config::$is3ds = filter_var(config('midtrans.is_3ds'), FILTER_VALIDATE_BOOLEAN);
+    }
+}
