e61169fa39476180f3c60a73dd604bdeceff5493
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-09-09 20:35:34 +0700

    modified:   resources/views/checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -6,8 +6,6 @@
 </head>
 
 <body>
-
-
     <h1>Checkout</h1>
     <input id="amount" type="number" value="100000">
     <button id="pay-button">Bayar Sekarang</button>
@@ -18,36 +16,40 @@
 
 
     <script>
-        document.getElementById('pay-button').addEventListener('click', function(){
-const amount = document.getElementById('amount').value;
+        document.getElementById('pay-button').addEventListener('click', function() {
+            const amount = document.getElementById('amount').value;
 
 
-fetch('/payment/create', {
-method: 'POST',
-headers: {
-'Content-Type': 'application/json',
-'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-},
-body: JSON.stringify({ amount })
-})
-.then(r => r.json())
-.then(data => {
-window.snap.pay(data.token, {
-onSuccess: function(result){
-alert('Payment success — server masih menunggu notification untuk final status.');
-},
-onPending: function(result){
-alert('Payment pending — silakan selesaikan pembayaran.');
-},
-onError: function(result){
-alert('Payment error');
-},
-onClose: function(){
-alert('Popup ditutup tanpa menyelesaikan pembayaran');
-}
-});
-});
-});
+            fetch('/payment/create', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute(
+                            'content')
+                    },
+                    body: JSON.stringify({
+                        amount
+                    })
+                })
+                .then(r => r.json())
+                .then(data => {
+                    window.snap.pay(data.token, {
+                        onSuccess: function(result) {
+                            alert(
+                                'Payment success — server masih menunggu notification untuk final status.');
+                        },
+                        onPending: function(result) {
+                            alert('Payment pending — silakan selesaikan pembayaran.');
+                        },
+                        onError: function(result) {
+                            alert('Payment error');
+                        },
+                        onClose: function() {
+                            alert('Popup ditutup tanpa menyelesaikan pembayaran');
+                        }
+                    });
+                });
+        });
     </script>
 </body>
 
